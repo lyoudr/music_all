7ae00a07bf8ebeff08adbe63ca9ca17c271e8881
Ann.Ke <ann.ke@cloud-interactive.com>
2021-12-28 14:18:25 +0800

    postgresql streaming replication => for table creation replication success

--- database/init_conf.sh
+++ database/init_conf.sh
@@ -5,3 +5,7 @@ psql -d "$POSTGRES_DB" -U "$POSTGRES_USER" <<-EOSQL
     CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_passwd';
     SELECT * FROM pg_create_physical_replication_slot('replication_slot_slave1');
 EOSQL
+
+# backup database
+echo "backup database"
+pg_basebackup -D /tmp/postgres_slave -S replication_slot_slave1 -X stream -P -U replicator -Fp -R
